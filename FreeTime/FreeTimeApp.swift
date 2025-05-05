a97475bddb90762f04cf85a736a3a13f36e51f39
Luana Gerber <luanagerber.dev@gmail.com>
2025-05-05 13:51:18 -0400

    Initial Commit

---
+++ FreeTime/FreeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeTimeApp.swift
+//  FreeTime
+//
+//  Created by Luana Gerber on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FreeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
