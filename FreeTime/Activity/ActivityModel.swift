7461313615be2fcc32af6a156fecef9ff3473d51
larry4us <89364723+larry4us@users.noreply.github.com>
2025-05-06 14:12:38 -0400

    feat(model): create User model for MVVM architecture

--- FreeTime/Activity/ActivityModel.swift
+++ FreeTime/Activity/ActivityModel.swift
@@ -5,3 +5,45 @@
 //  Created by Luana Gerber on 05/05/25.
 //
 
+import Foundation
+
+struct Child {
+    let id: Float
+    // let activityDuration: tempo
+    var hasStarted: Bool
+    var timeItWasStarted: Date?
+    var timeItWasFinished: Date?
+}
+
+struct Activity: Identifiable {
+    let id = UUID()
+    let name: String
+    var tags: [Tag]
+    var description: String
+    var necessaryMaterials: [String]
+    
+    // States
+    var activityState: ActivityState = .notStarted
+    
+    var startedAt: Date?
+    var FinishedAt: Date?
+    
+    // Information that will be provided by the responsible
+    var scheduledDate: Date?
+    var duration: TimeInterval?
+
+}
+
+// Os estados inProgress e completed carregam a data de início e fim da atividade
+enum ActivityState {
+    case notStarted
+    case inProgress //(Date)
+    case completed //(Date)
+}
+
+enum Tag {
+    case mentalExercise
+    case physicalExercise
+    case socialActivity
+    case study
+}
