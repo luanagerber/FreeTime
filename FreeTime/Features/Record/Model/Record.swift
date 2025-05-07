4579e0d40c8d4d9705b2c31c3ed04ffef2edb03f
Thales Souza <t.araujods@gmail.com>
2025-05-07 16:30:30 -0400

    feat: Record conforms with the Identifiable Protocol

--- FreeTime/Features/Record/Model/Record.swift
+++ FreeTime/Features/Record/Model/Record.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-struct Record {
+struct Record: Identifiable {
+    var id: UUID
     let child: Kid
     let parent: Parent
     let activity: Activity
@@ -15,14 +16,16 @@ struct Record {
     let duration: TimeInterval
     
     // States
-    var activityState: RecordState = .notStarted
+    var recordStatus: RecordState
 
-    init (child: Kid, parent: Parent,activity: Activity, date: Date, duration: TimeInterval) {
+    init (child: Kid, parent: Parent,activity: Activity, date: Date, duration: TimeInterval, recordStatus: RecordState) {
+        self.id = UUID()
         self.child = child
         self.parent = parent
         self.activity = activity
         self.date = date
         self.duration = duration
+        self.recordStatus = .notStarted
     }
 }
 
