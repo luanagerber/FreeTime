309ae0e6500cdeb59bbc4a563ef97c89aa13ad73
Thales Souza <t.araujods@gmail.com>
2025-05-22 15:08:35 -0400

    fix(organization): changed organization files

---
+++ FreeTime/Utilities/Helpers/Notification.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.swift
+//  FreeTime
+//
+//  Created by Maria Tereza Martins Pérez on 21/05/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let didAcceptCloudKitShare = Notification.Name("didAcceptCloudKitShare")
+}
