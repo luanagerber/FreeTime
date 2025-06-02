2ad7f46a62ea1500fa566739b174bd3c26f730aa
larry4us <89364723+larry4us@users.noreply.github.com>
2025-06-02 16:37:06 -0400

    fix(rewardsStoreView): as sombras nao sai mais cortadas na parte de baixo

--- FreeTime/Features/Rewards/View/RewardsStoreView.swift
+++ FreeTime/Features/Rewards/View/RewardsStoreView.swift
@@ -62,6 +62,7 @@ struct RewardsStoreView: View {
             }
             .scrollTargetLayout()
             .padding(.trailing, 36)
+            .padding(.bottom, 20)
         }
         .scrollTargetBehavior(.viewAligned)
         .scrollIndicators(.hidden)
