ad5a4df88bf01566d4402bc8933f86bc05396467
Ana Beatriz Seixas <aanabeatrizpintoseixas@gmail.com>
2025-06-05 16:16:39 -0400

    feat(ActivitiesCatalog): I changed the name of the image of the kid's book, because it was pulling the wrong reference.

--- FreeTime/Features/Activity/Model/ActivitiesCatalog.swift
+++ FreeTime/Features/Activity/Model/ActivitiesCatalog.swift
@@ -23,7 +23,7 @@ extension Activity {
             id: 1,
             name: "Ler livro",
             imageNameGenitor: "imageBookGenitor",
-            imageNameKid: "ImageBookKid",
+            imageNameKid: "imageBookKid",
             tags: [.mentalExercise, .study],
             description: "Ajude seu filho a escolher um livro que ele goste. Separe um lugar tranquilo e confortável. Se puder, leia junto com ele para tornar o momento mais especial.",
             kidDescription: "Escolha um livro que você goste ou tenha curiosidade de conhecer. Para isso encontre um cantinho tranquilo e leia com calma, prestando atenção na história e nos personagens.",
